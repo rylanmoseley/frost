7a786e73b111f3d24ca735205026eb6ff88d1940
Rylan <52055654+RDc12Programming@users.noreply.github.com>
2025-01-15 22:47:43 -0500

    Add unit test support

---
+++ src/test/java/EnableTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.simulation.DriverStationSim;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EnableTest {
+  @BeforeEach // this method will run before each test
+  void setup() {
+    assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
+  }
+
+  @Test // marks this method as a test
+  void simDSEnablesWhenEnabled() {
+    DriverStationSim.setEnabled(true);
+
+    assertEquals(true, DriverStationSim.getEnabled());
+  }
+}
