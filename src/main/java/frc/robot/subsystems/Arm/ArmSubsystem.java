f38247e4010cee54f4830fb4894e44e214d21720
Rylan <52055654+RDc12Programming@users.noreply.github.com>
2025-01-21 12:14:11 -0500

    Instantiate subsystem element

--- src/main/java/frc/robot/subsystems/Arm/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/Arm/ArmSubsystem.java
@@ -4,6 +4,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.function.DoubleSupplier;
 
 public class ArmSubsystem extends SubsystemBase {
+  private Claw m_claw = new Claw();
+  private Stage1 m_stage1 = new Stage1();
+  private Stage2 m_stage2 = new Stage2();
+  private Stage3 m_Stage3 = new Stage3();
 
-  public final DoubleSupplier simulationCurrentDraw = () -> 0;
+  public ArmSubsystem() {}
+
+  public final DoubleSupplier totalCurrentDraw = () -> 0;
 }
