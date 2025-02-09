d233b6c540f715504c995dd8ac338d3492dc1dc1
rylanmoseley <52055654+rylanmoseley@users.noreply.github.com>
2025-02-09 04:28:59 +0000

    Run formatter

--- src/main/java/frc/robot/utilities/Power.java
+++ src/main/java/frc/robot/utilities/Power.java
@@ -5,61 +5,62 @@ import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import frc.robot.Constants.PowerConstants;
 
 public class Power {
-    private static Power powerInstance = null;
+  private static Power powerInstance = null;
 
-    private PowerDistribution m_pdh;
-    private Power() {
-        m_pdh = new PowerDistribution(PowerConstants.CAN.PDH, ModuleType.kRev);
-    }
+  private PowerDistribution m_pdh;
 
-    private static synchronized Power getInstance() {
-        if (powerInstance == null) {
-            powerInstance = new Power();
-        }
-        return powerInstance;
-    }
+  private Power() {
+    m_pdh = new PowerDistribution(PowerConstants.CAN.PDH, ModuleType.kRev);
+  }
 
-    public static double getVoltage() {
-        return getInstance().m_pdh.getVoltage();
+  private static synchronized Power getInstance() {
+    if (powerInstance == null) {
+      powerInstance = new Power();
     }
+    return powerInstance;
+  }
 
-    public static double getCurrent() {
-        return getInstance().m_pdh.getTotalCurrent();
-    }
+  public static double getVoltage() {
+    return getInstance().m_pdh.getVoltage();
+  }
 
-    public static double getTemperature() {
-        return getInstance().m_pdh.getTemperature();
-    }
-    
-    public static double getCurrent(int channel) {
-        return getInstance().m_pdh.getCurrent(channel);
-    }
+  public static double getCurrent() {
+    return getInstance().m_pdh.getTotalCurrent();
+  }
 
-    public static double[] getAllCurrents() {
-        return getInstance().m_pdh.getAllCurrents();
-    }
+  public static double getTemperature() {
+    return getInstance().m_pdh.getTemperature();
+  }
 
-    public static double getTotalPower() {
-        return getInstance().m_pdh.getTotalPower();
-    }
+  public static double getCurrent(int channel) {
+    return getInstance().m_pdh.getCurrent(channel);
+  }
 
-    public static double getTotalEnergy() {
-        return getInstance().m_pdh.getTotalEnergy();
-    }
+  public static double[] getAllCurrents() {
+    return getInstance().m_pdh.getAllCurrents();
+  }
 
-    public static boolean getSwitchableChannel() {
-        return getInstance().m_pdh.getSwitchableChannel();
-    }
+  public static double getTotalPower() {
+    return getInstance().m_pdh.getTotalPower();
+  }
 
-    public static void setSwitchableChannel(boolean enabled) {
-        getInstance().m_pdh.setSwitchableChannel(enabled);
-    }
+  public static double getTotalEnergy() {
+    return getInstance().m_pdh.getTotalEnergy();
+  }
 
-    public static void enableUnderglow () {
-        setSwitchableChannel(true);
-    }
+  public static boolean getSwitchableChannel() {
+    return getInstance().m_pdh.getSwitchableChannel();
+  }
 
-    public static void disableUnderglow () {
-        setSwitchableChannel(false);
-    }
+  public static void setSwitchableChannel(boolean enabled) {
+    getInstance().m_pdh.setSwitchableChannel(enabled);
+  }
+
+  public static void enableUnderglow() {
+    setSwitchableChannel(true);
+  }
+
+  public static void disableUnderglow() {
+    setSwitchableChannel(false);
+  }
 }
